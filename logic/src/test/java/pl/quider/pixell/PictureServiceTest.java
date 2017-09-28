e58167de12e55bc45d2e33e83b59415c34384d29
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2017-09-28 22:45:51 +0200

    PictureServiceTest

---
+++ logic/src/test/java/pl/quider/pixell/PictureServiceTest.java
@@ -0,0 +1,28 @@
+package pl.quider.pixell;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PictureServiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findColorMap() throws Exception {
+    }
+
+    @Test
+    public void findTileColorOnMosaic() throws Exception {
+    }
+
+    @Test
+    public void countTileAverageImage() throws Exception {
+    }
+
+}
