9a6b7f8feec947afa5fd6c05f2639b32682eadb1
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2017-10-02 23:09:54 +0200

    test for prepare main picture

---
+++ logic/src/test/java/pl/quider/pixell/services/PrepareMainPictureTest.java
@@ -0,0 +1,21 @@
+package pl.quider.pixell.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrepareMainPictureTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void call() throws Exception {
+    }
+
+}
