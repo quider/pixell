949912c9340b2b51c72782ca86506a8ac0613754
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2019-10-05 21:44:14 +0200

    Added log to menu creation

--- gui/src/main/java/pl/quider/standalone/mainwindow/MainWindowController.java
+++ gui/src/main/java/pl/quider/standalone/mainwindow/MainWindowController.java
@@ -3,12 +3,14 @@ package pl.quider.standalone.mainwindow;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuBar;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Component
+@Slf4j
 public class MainWindowController implements Initializable {
 
     @FXML
@@ -17,6 +19,7 @@ public class MainWindowController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        log.debug("Setting menu as system Menu Bar {}", true);
         menuBar.setUseSystemMenuBar(true);
     }
 }
